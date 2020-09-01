1626fc630003e194f7e3b661ca1916c388065c77
root <root@LAPTOP-CNSTAQB5.localdomain>
2020-09-01 01:08:51 -0500

    environment settings

---
+++ Src/Components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+//const HelloWorld = () => {
+//  return <h1>Hola Mundo</h1>
+//};
+
+//-----es lo mismo
+const HelloWorld = () => (
+  <h1>Hola Mundo</h1>
+);
+
+export default HelloWorld;
